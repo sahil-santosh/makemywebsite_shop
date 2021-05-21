2d913b82dba24b14ae68ac2c945a589785c2387b
sahil-santosh <sahilbudania@gmail.com>
2021-05-21 09:46:08 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/makemywebsite_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.makemywebsite_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:makemywebsite_shop/screens/product_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.orange,
+      ),
+      home: ProductOverviewScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Let\'s build a shop!'),
+      ),
+    );
+  }
+}
