a6d9ae57b0563327cebaef6f7635847297f773ed
sahil-santosh <sahilbudania@gmail.com>
2021-05-22 12:21:15 +0530

    using consumer and set listen false

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -4,19 +4,10 @@ import 'package:makemywebsite_shop/screens/product_detail_screen.dart';
 import 'package:provider/provider.dart';
 
 class ProductItem extends StatelessWidget {
-  // final String id;
-  // final String title;
-  // final String imageUrl;
-
-  // ProductItem(
-  //   this.id,
-  //   this.title,
-  //   this.imageUrl,
-  // );
-
+  
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);            
+    final product = Provider.of<Product>(context, listen: false);            
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -40,10 +31,12 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).accentColor,
           ),
           title: Text(product.title, textAlign: TextAlign.center),
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
-            color: Theme.of(context).accentColor,
+          trailing: Consumer<Product>(
+            builder: (context, value, _) => IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
           ),
         ),
       ),
