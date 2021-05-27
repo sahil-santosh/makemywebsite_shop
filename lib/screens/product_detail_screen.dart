f5366696ac2a07a6e8f96b47fc10b074ae4ca409
sahil-santosh <sahilbudania@gmail.com>
2021-05-27 10:47:41 +0530

    day12 working on product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -13,6 +13,25 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(loadedProduct.imageUrl,fit: BoxFit.cover,),
+            ),
+            SizedBox(height: 10,),
+            Text('\Rs. ${loadedProduct.price}',style: TextStyle(fontSize: 20),),
+            SizedBox(height: 10,),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(loadedProduct.desc,textAlign: TextAlign.center,softWrap: true),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
